38f666b07b4b5e3fbc480221ae2e61d17d49d9a4
Sam Gossie <sjg3609@gmail.com>
2023-01-25 17:36:28 -0600

    Updated answers 2-5 of homework assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -8,6 +8,7 @@ console.log('***** Function Practice *****')
 function hello() {
   return 'Hello World!';
 }
+
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
@@ -15,41 +16,55 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return `Hello, ${name}`;
 }
 // Remember to call the function to test
 
+console.log(helloName('Sam'));
+console.log(helloName('Nollie'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber ) {
+  return firstNumber + secondNumber;
 }
 
 
+console.log(addNumbers(1, 2));
+
+console.log(addNumbers(10, 7));
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( firstNumber, secondNumber, thirdNumber ){
+  return firstNumber * secondNumber * thirdNumber;
 }
+
+console.log(multiplyThree(2, 4, 6));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
+  } else {
+    return false;
   }
-    return;
 }
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
+console.log(isPositive(7));
+console.log(isPositive(-3));
+console.log(isPositive(140));
+
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
+    
 
 }
 
